ca2755e954ecb8acabb69139a2b76b7725c54f9d
Sabrina McField <sabrinamcfield@gmail.com>
2025-01-21 16:27:32 -0500

    price analysis histogram added

--- car_app.py
+++ car_app.py
@@ -30,3 +30,16 @@ actual_range = list(range(year_range[0],year_range[1]+1))
 df_filtered = df[ (df.model == selected_menu) & (df.model_year.isin(list(actual_range)))]
 
 df_filtered
+
+st.header('Price Analysis')
+st.write("""
+##### Lets analyze the what factor influences the price the most. We will consider how distribution of price varies depending on transmission, type, fuel and condition
+""")
+
+list_for_hist = ['transmission','type','condition', 'fuel']
+
+selected_type = st.selectbox('Select for price distribution', list_for_hist)
+
+fig1 = px.histogram(df, x='price', color= selected_type)
+fig1.update_layout(title='<b>Split of price by {}<b>'.format(selected_type))
+st.plotly_chart(fig1)
